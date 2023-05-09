04c7288a600d3108bcfe0c44763241ad89c80dc3
SURAJpadhi8 <127823106+SURAJpadhi8@users.noreply.github.com>
2023-05-09 10:40:36 +0530

    Add files via upload
    This is the solution of 2 Question

---
+++ BinaryTree/src/com/greatlearning/main/Node.java
@@ -0,0 +1,14 @@
+package com.greatlearning.main;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+	
+	public Node(int data)
+	{
+		this.left=null;
+		this.data=data;
+		this.right=null;
+	}
+}
