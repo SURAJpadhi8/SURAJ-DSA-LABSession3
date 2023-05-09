d8d5ec9e2dfbf939acf2d2c7e598a61264d195e3
SURAJpadhi8 <127823106+SURAJpadhi8@users.noreply.github.com>
2023-05-09 10:38:14 +0530

    Add files via upload
    These is the solution of 1 Question

---
+++ LabSession3/src/com/greatlearning/driver/DriverApp.java
@@ -0,0 +1,56 @@
+package com.greatlearning.driver;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DriverApp {
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the Expression:-");
+			String input=sc.next();
+			if(checkExpression(input))
+				System.out.println("The Entered strings has balanced Brackets");
+			else
+				System.out.println("The Entered strings do not contain balanced Brackets");
+		}
+		private static boolean checkExpression(String input) {
+			Stack<Character> stack=new Stack<>();
+			for(char k:input.toCharArray())
+			{
+				if(k=='{' || k=='[' || k=='(')
+				{
+					stack.push(k);
+					continue;
+				}
+				
+				if(stack.isEmpty())
+				return false;
+				
+				char z;
+				switch(k)
+				{
+						case '}':
+									z=stack.pop();
+									if(z=='[' || z=='(')
+									return false;
+									break;
+						
+						case ']':
+									z=stack.pop();
+									if(z=='{' || z=='(')
+									return false;
+									break;
+									
+						case ')':
+									z=stack.pop();
+									if(z=='{' || z=='[')
+									return false;
+									break;
+				}
+				
+				
+			}
+			
+			return(stack.isEmpty());
+		}
+}
